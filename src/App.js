42d4c20eb49fbabf68127123bb8ed4286113f88b
Bhoomika <bhoomikahm18@gmail.com>
2023-11-17 12:13:53 +0530

    Material UI Dialog Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 // import Test2 from "./Test2";
 // import Test3 from "./Test3";
-import Test4 from "./Test4";
+// import Test4 from "./Test4";
+import Test5 from "./Test5";
 // import Text from "./Text";
 
 function App() {
   return (
     <div >
-      <Test4 />
+      <Test5 />
     </div>
   );
 }
