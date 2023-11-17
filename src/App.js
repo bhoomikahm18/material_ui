bf260cc8b39c4e87a031526281cb9bea1c167e09
Bhoomika <bhoomikahm18@gmail.com>
2023-11-17 12:22:15 +0530

    Material UI Modal Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 // import Test2 from "./Test2";
 // import Test3 from "./Test3";
 // import Test4 from "./Test4";
-import Test5 from "./Test5";
+// import Test5 from "./Test5";
+import Test6 from "./Test6";
 // import Text from "./Text";
 
 function App() {
   return (
     <div >
-      <Test5 />
+      <Test6 />
     </div>
   );
 }
