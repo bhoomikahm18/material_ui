bb4f5ca0e0eb9d5140aa51d4f777348f77e81eb5
Bhoomika <bhoomikahm18@gmail.com>
2023-11-17 12:00:38 +0530

    Material UI Cards Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 // import Test2 from "./Test2";
-import Test3 from "./Test3";
+// import Test3 from "./Test3";
+import Test4 from "./Test4";
 // import Text from "./Text";
 
 function App() {
   return (
     <div >
-      <Test3 />
+      <Test4 />
     </div>
   );
 }
