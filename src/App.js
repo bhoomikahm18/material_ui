f59b4855a4658902ed3b82676fe28f710f6c7ffc
Bhoomika <bhoomikahm18@gmail.com>
2023-11-18 11:01:26 +0530

    Adding grid and tabs inside a appbar

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@
 // import Test5 from "./Test5";
 // import Test6 from "./Test6";
 // import Test7 from "./Test7";
-import Layout from "./Layout";
+// import Layout from "./Layout";
+import Navbar from "./components/Navbar";
 // import Test8 from "./Test8";
 // import Text from "./Text";
 
 function App() {
   return (
     <div >
-      <Layout />
+      <Navbar />
     </div>
   );
 }
